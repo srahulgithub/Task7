6be0e28d56dbbe4edbf8849de54747112aafe7a5
srahulgithub <75783819+srahulgithub@users.noreply.github.com>
2023-03-10 02:37:40 +0530

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Assignment7.Models;
+using Assignment7.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Assignment7.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly UserService _usersService;
+
+    public UserController(UserService usersService) =>
+        _usersService = usersService;
+
+    [HttpGet]
+    public async Task<List<User>> Get() =>
+        await _usersService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<User>> Get(string id)
+    {
+        var user = await _usersService.GetAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(User newUser)
+    {
+        await _usersService.CreateAsync(newUser);
+
+        return CreatedAtAction(nameof(Get), new { id = newUser._id }, newUser);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, User updatedUser)
+    {
+        var user = await _usersService.GetAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        updatedUser._id = user._id;
+
+        await _usersService.UpdateAsync(id, updatedUser);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var user = await _usersService.GetAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        await _usersService.RemoveAsync(id);
+
+        return NoContent();
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Assignment7.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string _id { get; set; }
+
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")]
+    public string Name { get; set; }
+
+    public string Country { get; set; }
+
+    public double AnnualIncome { get; set; }
+
+    public List<string> EmailIdsList { get; set; }
+}
